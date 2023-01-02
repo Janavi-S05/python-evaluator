5a023b4d86815dadfaebf2d5bca7a03e0f013f8b
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-01-02 16:54:00 +0530

    Update Leaderboard.js

--- src/screen/Leaderboard.js
+++ src/screen/Leaderboard.js
@@ -27,7 +27,7 @@ export default function Leaderboard()
         </ul>
         </div>
   <div className="board-container">
-  <div className="myblock">
+  <div className="myblock1">
   <table>
     <thead>
       <tr>
@@ -43,52 +43,11 @@ export default function Leaderboard()
           <div>
           <img
         src="https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-        alt="car" style={{width:40,borderRadius: 35}}
-      />
+        alt="car" style={{width:40,borderRadius: 35}}/>
           <p>Lani</p>
           </div>
         </td>
         <td>0:16:23</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>
-        <div>
-        <img
-        src="https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-        alt="car" style={{width:40,borderRadius: 35}}
-      />
-          <p>Israel</p>
-        </div>
-        </td>
-        <td>0:20:30</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>
-          <div>
-          <img
-        src="https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-        alt="car" style={{width:40,borderRadius: 35}}
-      />
-            <p>Eveleen</p>
-          </div>
-          </td>
-        <td>0:29:41</td>
-        
-        
-      </tr>
-      <tr>
-        <td>4</td>
-        <td><div>
-        <img
-        src="https://t4.ftcdn.net/jpg/00/65/77/27/240_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg"
-        alt="car" style={{width:40,borderRadius: 35}}
-      />
-          <p>Conn</p>
-        </div>
-        </td>
-        <td>0:30:45</td>
       </tr>
       </tbody>
       </table>
